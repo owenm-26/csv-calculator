e3a793fd3bb538debe706c140fabc598edf84597
Owen Mariani <mariani.owen@gmail.com>
2024-01-24 12:13:56 -0500

    finished basic Calc class and commented CSVCalc

--- CSVCalculatorHandler.py
+++ CSVCalculatorHandler.py
@@ -1,15 +1,28 @@
 class CSVCalculatorHandler:
     def __init__(self):
         pass
+        # array variable to hold unsaved history ?
 
     def process_csvs(self, filenames, output_prefix="output"):
         pass
+        # create output csv
+        # read input csv
+            # store numbers (operands) in an array
+            # checks if operator is a thing
+                # if it is then call method and store result
+                # assign error code 
+            # write to file called "output_prefix_<file_name>"
+    
+        # assign necessary variables and call save_to_history()
 
     def save_to_history(self, filename, operation, result, error_code):
         pass
+        # write method inputs to the global variable 
 
     def history_export(self, export_filename):
         pass
+        # write (without erasing)? the global variable to a "export_filename" (history.txt)
+        # clear global variable
 
 
 if __name__ == '__main__':
--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,30 @@
 class Calculator:
-    pass
+    def add(operands):
+        result = operands[0]
+        for i in operands[1:]: #skip the first operand because it is assigned to result already
+            result += i
+        return result
+    
+    def subtract(operands):
+        result = operands[0]
+        for i in operands[1:]: #skip the first operand because it is assigned to result already
+            result -= i
+        return result
+
+    def multiply(operands):
+        result = operands[0]
+        for i in operands[1:]: #skip the first operand because it is assigned to result already
+            result *= i
+        return result
+
+    def divide(operands):
+        result = operands[0]
+        for i in operands[1:]: #skip the first operand because it is assigned to result already
+            result /= i
+        return result
+
+    def exponentiate(operands):
+        result = operands[0]
+        for i in operands[1:]: #skip the first operand because it is assigned to result already
+            result **= i
+        return result
