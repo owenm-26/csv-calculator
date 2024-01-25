05811a6a2887125877ba3cb0e1ada1c7168af2b7
Owen Mariani <mariani.owen@gmail.com>
2024-01-25 14:34:49 -0500

    CSVCalcHandler methods complete, need to do main

--- CSVCalculatorHandler.py
+++ CSVCalculatorHandler.py
@@ -1,29 +1,97 @@
+import csv
+import numpy as np
+from Calculator import Calculator
+validMethods = ["add", "subtract", "multiply", "divide", "exponentiate"]
 class CSVCalculatorHandler:
+    
     def __init__(self):
-        pass
-        # array variable to hold unsaved history ?
+        self.unsavedHistory = np.asarray(['input_file','operation','result','error']) # array variable to hold unsaved history ? 
 
     def process_csvs(self, filenames, output_prefix="output"):
-        pass
-        # create output csv
-        # read input csv
-            # store numbers (operands) in an array
-            # checks if operator is a thing
-                # if it is then call method and store result
-                # assign error code 
-            # write to file called "output_prefix_<file_name>"
-    
-        # assign necessary variables and call save_to_history()
+        # iterate through each file
+        for file in filenames:
+            with open(file, 'r') as csvfile:
+                oneFile = csv.reader(csvfile)
+
+            # create a new CSV to write to throughout loop of single file
+            outputFileName = output_prefix + file
+            with open(outputFileName, 'w'):
+                writer = csv.writer(file)
+                writer.writerows([['result', 'error']])
+            
+            # iterate through each row of the CSV selected
+            for row in oneFile:
+                i = 0 #counter
+
+                # instantiate inputs to function
+                params = []
+                method = ""
+                error = 0
+                result = ""
+                
+                # assign params var
+                while(not row[i].isalpha()):
+                    params += [int(row[i])] #cast to int and add to params list
+                    i+=1
+                if(len(params) < 2): #make sure the call has enough params
+                    error = 2
+                    print("Bad Number of Params")
+                
+                # assign method var
+                method = row[i] 
+                if(method not in validMethods):
+                    error = 3
+                    print("Unknown Operator")
+                
+                try:
+                    calc = Calculator()
+                    # NEED TO CALL FUNCTION HERE (switch statement)
+                    if(method.equals("add")):
+                        result = calc.add(params)
+                    elif(method.equals("subtract")):
+                        result = calc.subtract(params)
+                    elif(method.equals("multiply")):
+                        result = calc.multiply(params)
+                    elif(method.equals("divide")):
+                        result = calc.divide(params)
+                    else:
+                        result = calc.exponentiate(params)
+                #Error handling
+                except ZeroDivisionError:
+                    error = 1
+                    print("Divided by Zero")
+
+                except:
+                    error = 4
+                    print("Unknown Error")
+                
+                # write new row to file
+                with open(outputFileName, 'a') as workingFile:
+                    writer = csv.writer(workingFile)
+                    writer.writerows([result, error])
+                    print('Row written: Method=' + method + ", params=" + params + ", \n**result=" + result + ", error=" + error)
+                self.save_to_history(file, method, result, error)
 
     def save_to_history(self, filename, operation, result, error_code):
-        pass
         # write method inputs to the global variable 
+        self.unsavedHistory = np.append(self.unsavedHistory, [filename,operation,result,error_code])
 
     def history_export(self, export_filename):
-        pass
-        # write (without erasing)? the global variable to a "export_filename" (history.txt)
-        # clear global variable
+        with open(export_filename, 'w'):
+                writer = csv.writer(export_filename)
+                writer.writerows([self.unsavedHistory])
 
 
 if __name__ == '__main__':
+
     print("write some code, your testing software will call this main function")
+
+
+
+
+
+
+####### General Sources ########
+## CSV Library Documentation: https://docs.python.org/3/library/csv.html
+## CSV Examples: https://www.geeksforgeeks.org/writing-csv-files-in-python/ 
+## Numpy Array to CSV: https://stackoverflow.com/questions/6081008/dump-a-numpy-array-into-a-csv-file
