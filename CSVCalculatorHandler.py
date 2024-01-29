e45ccffac3cf1516b1ebec948ecdf91103059036
Owen Mariani <mariani.owen@gmail.com>
2024-01-29 11:42:59 -0500

    appending version - not working

--- CSVCalculatorHandler.py
+++ CSVCalculatorHandler.py
@@ -1,74 +1,84 @@
+#!/usr/bin/env python3
 import csv
+import sys
 import numpy as np
 from Calculator import Calculator
-validMethods = ["add", "subtract", "multiply", "divide", "exponentiate"]
 class CSVCalculatorHandler:
     
     def __init__(self):
-        self.unsavedHistory = np.asarray(['input_file','operation','result','error']) # array variable to hold unsaved history ? 
+        self.unsavedHistory = np.array(['input_file','operation','result','error']) # array variable to hold unsaved history ? 
 
     def process_csvs(self, filenames, output_prefix="output"):
+        validMethods = ["add", "subtract", "multiply", "divide", "exponentiate"]
         # iterate through each file
         for file in filenames:
-            with open(file, 'r') as csvfile:
-                oneFile = csv.reader(csvfile)
-
             # create a new CSV to write to throughout loop of single file
             outputFileName = output_prefix + file
-            with open(outputFileName, 'w'):
-                writer = csv.writer(file)
+            with open(outputFileName, 'w', newline='') as outputFile:
+                writer = csv.writer(outputFile)
                 writer.writerows([['result', 'error']])
-            
-            # iterate through each row of the CSV selected
-            for row in oneFile:
-                i = 0 #counter
 
-                # instantiate inputs to function
-                params = []
-                method = ""
-                error = 0
-                result = ""
-                
-                # assign params var
-                while(not row[i].isalpha()):
-                    params += [int(row[i])] #cast to int and add to params list
-                    i+=1
-                
-                # assign method var
-                method = row[i] 
-                if(method not in validMethods):
-                    error = 3
-                    print("Unknown Operator")
-                
-                try:
-                    calc = Calculator()
-                    # NEED TO CALL FUNCTION HERE (switch statement)
-                    if(method == ("add")):
-                        result = calc.add(params)
-                    elif(method == ("subtract")):
-                        result = calc.subtract(params)
-                    elif(method == ("multiply")):
-                        result = calc.multiply(params)
-                    elif(method ==("divide")):
-                        result = calc.divide(params)
-                    else:
-                        result = calc.exponentiate(params)
+            #open first file
+            with open(file, 'r') as csvfile:
+                oneFile = csv.reader(csvfile)
+    
+                # iterate through each row of the CSV selected
+                for row in oneFile:
+                    print('----------')
+                    print(row)
+                    i = 0 #counter
 
-                #Error handling
-                except:
-                    error = 4
-                    print("Unknown Error")
+                    # instantiate inputs to function
+                    params = []
+                    method = ""
+                    error = 0
+                    result = ""
+                    
+                    # assign params var
+                    for arg in row[:-1]:
+                        params += [float(arg)] #cast to float and add to params list
+                   
+                    # assign method var
+                    method = row[-1] #it should be the last argument
+                    print(params, method)
+
+                    print(method in validMethods)
+                    if(method not in validMethods):
+                        error = 3
+                        print("Unknown Operator")
+                    
+                if(error == 0):
+                    try:
+                        calc = Calculator()
+                        # NEED TO CALL FUNCTION HERE (switch statement)
+                        if(method == ("add")):
+                            result = calc.add(params)
+                        elif(method == ("subtract")):
+                            result = calc.subtract(params)
+                        elif(method == ("multiply")):
+                            result = calc.multiply(params)
+                        elif(method ==("divide")):
+                            result = calc.divide(params)
+                        else:
+                            result = calc.exponentiate(params)
+                        print("RESULT:", result)
+
+                    #Error handling
+                    except:
+                        error = 4
+                        print("Unknown Error")
                 
                 # write new row to file
-                with open(outputFileName, 'a') as workingFile:
+                with open(outputFileName, 'a', newline='') as workingFile:
                     writer = csv.writer(workingFile)
-                    writer.writerows([result, error])
-                    print('Row written: Method=' + method + ", params=" + params + ", \n**result=" + result + ", error=" + error)
-                self.save_to_history(file, method, result, error)
+                    writer.writerows([[result, error]])
+                    print('Row written: Method=', method, ", params=", params, ", \n**result=", result, ", error=", error)
+                self.save_to_history(self, filename=file, operation=method, result=result, error_code=error)
 
     def save_to_history(self, filename, operation, result, error_code):
         # write method inputs to the global variable 
-        self.unsavedHistory = np.append(self.unsavedHistory, [filename,operation,result,error_code])
+        handler = CSVCalculatorHandler()
+        handler.unsavedHistory = np.append(handler.unsavedHistory, [filename,operation,result,error_code])
 
     def history_export(self, export_filename):
         with open(export_filename, 'w'):
@@ -201,7 +211,18 @@ class CSVCalculatorHandler:
 
 if __name__ == '__main__':
     cch = CSVCalculatorHandler
-    cch.unitTests()
+    #cch.unitTests()
+
+    if(len(sys.argv) < 2):
+        print('Need at least 1 csv to process')
+    else:
+        # Extract file names from command-line arguments
+        args = []
+        for arg in sys.argv[1:]:
+            args += [arg]
+        
+        cch.process_csvs(self=cch, filenames=args, output_prefix='output_test_')
+        
 
 
 
@@ -210,3 +231,4 @@ if __name__ == '__main__':
 ## CSV Library Documentation: https://docs.python.org/3/library/csv.html
 ## CSV Examples: https://www.geeksforgeeks.org/writing-csv-files-in-python/ 
 ## Numpy Array to CSV: https://stackoverflow.com/questions/6081008/dump-a-numpy-array-into-a-csv-file
+## Command line argument lesson: https://machinelearningmastery.com/command-line-arguments-for-your-python-script/ 
